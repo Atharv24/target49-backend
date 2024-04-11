54aa78fd6bbd250f63048d042b805771e0a3e287
Atharv Maan <atharv24@live.in>
2024-04-10 22:32:18 -0400

    minor fix

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/atharv24/target49server/server"
+	"github.com/atharv24/target49server/tcp_server"
 )
 
 func main() {
     port := "42069"
-    server := server.NewServer(":"+port)
+    server := tcp_server.NewServer(":"+port)
     err := server.Start()
     if err != nil {
         fmt.Println("Error listening:", err.Error())
