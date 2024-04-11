2c2b4b1a734231582f49824a2e523dce9af9dc10
Atharv Maan <atharv24@live.in>
2024-04-10 22:32:56 -0400

    Revert "minor fix"
    This reverts commit 54aa78fd6bbd250f63048d042b805771e0a3e287.

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/atharv24/target49server/tcp_server"
+	"github.com/atharv24/target49server/server"
 )
 
 func main() {
     port := "42069"
-    server := tcp_server.NewServer(":"+port)
+    server := server.NewServer(":"+port)
     err := server.Start()
     if err != nil {
         fmt.Println("Error listening:", err.Error())
